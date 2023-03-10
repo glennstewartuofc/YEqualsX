f625f7bac9e4d194100055dc2a65c5aa454d9911
glennstewartuofc <127443584+glennstewartuofc@users.noreply.github.com>
2023-03-10 13:05:06 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,16 +2,33 @@ let times = 0
 let count = 0
 input.onButtonPressed(Button.A, function () {
     times = pin()
-    y_equals_x(times)
+    if (times == 1) {
+        y_equals_x(times, true)
+    } else {
+        if (times == 2) {
+            y_equals_x(times, true)
+        } else {
+            if (times == 3) {
+                y_equals_x(times, true)
+            }
+        }
+    }
 })
-function y_equals_x (num: number) {
+function y_equals_x (num: number, bool: boolean) {
     for (let index = 0; index < num; index++) {
         count = 0
         for (let index = 0; index < 5; index++) {
-            led.plot(count, 4 - count)
-            basic.pause(200)
-            led.unplot(count, 4 - count)
-            count += 1
+            if (bool) {
+                led.plot(count, 4 - count)
+                basic.pause(200)
+                led.unplot(count, 4 - count)
+                count += 1
+            } else {
+                led.plot(4 - count, count)
+                basic.pause(200)
+                led.unplot(4 - count, count)
+                count += 1
+            }
         }
     }
 }
@@ -31,6 +48,6 @@ function pin () {
     }
 }
 basic.forever(function () {
-    serial.writeValue("Celcius", input.temperature())
-    basic.pause(1000)
+    led.toggle(4, 4)
+    basic.pause(500)
 })
